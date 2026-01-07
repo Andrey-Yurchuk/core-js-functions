4cbd982e7e31e3383b1fa720a503d400b0e51d9a
Andrey Yurchuk <r1430@tut.by>
2026-01-07 21:40:29 +0300

    feat: implement memoize function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -121,8 +121,17 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cachedResult;
+  let hasBeenCalled = false;
+
+  return function memoized() {
+    if (!hasBeenCalled) {
+      cachedResult = func();
+      hasBeenCalled = true;
+    }
+    return cachedResult;
+  };
 }
 
 /**
